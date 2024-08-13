425f362f9d89dce6cf146b58c5ed22df61b0d5e6
Chonkz90 <zahlerdal@yahoo.com>
2024-08-13 12:30:31 +0200

    3/4ths of a working calculator

--- script.js
+++ script.js
@@ -1,5 +1,57 @@
 // if/else statement
 
-const hour = 10;
+// operator
+/*
+= betyr å erklære noe
+== betyr samme verdi (lite brukt)
+=== betyr samme verdi og samme datatype
 
-if
+< betyr mindre enn (less than)
+> betyr større enn ( more than)
+
+&& betyr og(and)
+|| betyr eller (or)
+*/
+
+const hour = 20;
+
+// Sjekker om klokken er mindre enn 12
+if (hour < 12) {
+    console.log ("Good morning")
+} else if (hour === 12) {
+    // Sjekker om klokken er akkurat 12
+console.log (" The clock is 12!")
+} else if (hour > 14 && hour < 18) {
+    // sSjekker om klokken er mellom 14 og 18.
+    console.log("The clock is between 14 and 18")
+}else {
+    // Tidspunkt som ikke passer før 12 eller akkurat 12
+    console.log ("Good day!")
+};
+
+// 
+// 
+
+// En enkel kalkulator / Simple Calculator
+
+const number1 = 10;
+const number2 = 5;
+let sum;
+
+if (number1 + number2) {
+    sum = number1 + number2
+    console.log (sum)
+} else if (number1 - number2) {
+    sum = number1 - number2
+    console.log(sum)
+} else if (number1 * number2) {
+    sum = number1 * number2
+    console.log(sum)
+} else if (number1 / number2){
+    sum = number1 / number2
+    console.log(sum)
+} else {
+    sum = "Kan ikke kalkulere"
+    console.log(sum)
+}
+
